4e902c3ad969ab17498bf54f7068ea2171d87b8b
cratuss <ldoh92@gmail.com>
2022-08-05 16:43:38 +0900

    Add: mission5완료

--- dongho/js/main.js
+++ dongho/js/main.js
@@ -1,6 +1,40 @@
 let comment_unstaged = document.getElementsByClassName("input-comments");
 let feedCommentsLeft = document.getElementsByClassName("feed-comments-left");
-let buttonUpload = document.getElementsByClassName("feed-comments-plus-name");
+let buttonUpload = document.getElementsByClassName("input-comments-button");
+let jsonUserName = document.getElementsByClassName("userName");
+let jsonUserImage = document.getElementsByClassName("userImage");
+let jsonFeedImage = document.getElementsByClassName("feedImage");
+let jsonCountLiked = document.getElementsByClassName("countLiked");
+let jsonFeedTime = document.getElementsByClassName("feedTime");
+
+fetch("./data/mock.json", { method: "GET" })
+  .then((res) => res.json())
+  .then((data) => {
+    let jsonFeeds = data.feeds;
+    jsonFeeds.forEach((element, index) => {
+      jsonUserName[index].textContent = element.userName;
+      jsonUserImage[index].src = element.userImage;
+      jsonFeedImage[index].src = element.feedImage;
+      jsonCountLiked[index].textContent = element.countLiked;
+      jsonFeedTime[index].textContent = element.feedTime;
+
+      for (let i = 0; i < 2; i++) {
+        let newDiv = document.createElement("div");
+        let newSpan1 = document.createElement("span");
+        let newSpan2 = document.createElement("span");
+        newSpan1.className = "feed-favor-name";
+        newSpan1.style.fontWeight = "bold";
+        newSpan1.textContent = element.comments[i].user;
+
+        newSpan2.className = "feed-favor-name";
+        newSpan2.textContent = element.comments[i].content;
+
+        newDiv.append(newSpan1);
+        newDiv.append(newSpan2);
+        feedCommentsLeft[index].append(newDiv);
+      }
+    });
+  });
 
 Array.from(buttonUpload).forEach((element, index) => {
   element.addEventListener("click", function () {
@@ -11,6 +45,9 @@ Array.from(buttonUpload).forEach((element, index) => {
       upload(comment, writer, index);
       comment_unstaged[index].value = "";
     }
+
+    buttonUpload[index].style.color = "rgb(187, 187, 216)";
+    buttonUpload[index].style.cursor = "default";
   });
 });
 
@@ -19,11 +56,15 @@ Array.from(comment_unstaged).forEach((element, index) => {
     let comment = element.value;
     let writer = "lee";
     buttonUpload[index].style.color = "blue";
+    buttonUpload[index].style.cursor = "pointer";
     if (event.keyCode === 13 && comment !== "") {
       upload(comment, writer, index);
       comment_unstaged[index].value = "";
+      buttonUpload[index].style.color = "rgb(187, 187, 216)";
+      buttonUpload[index].style.cursor = "default";
     } else if (comment === "") {
       buttonUpload[index].style.color = "rgb(187, 187, 216)";
+      buttonUpload[index].style.cursor = "default";
     }
   });
 });
